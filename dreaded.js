da487470c042093570a81a4fcb14178b6994887e
Re-Jeong01 <rejeongguanxii@gmail.com>
2024-10-16 18:04:25 +0200

    Update dreaded.js

--- dreaded.js
+++ dreaded.js
@@ -1300,63 +1300,33 @@ mediaUrl: anup3k.url,
 }
 break;
  
-  case 'play':
-    case 'stream': {
-     if (!text) {
-            m.reply('What song do you want to download?')
-            return;
+case "play":{
+	if (!text) return reply('Please provide a song name!');
+	const randomReduction = Math.floor(Math.random() * 5) + 1;
+	const yts = require("youtube-yts");
+	let search = await yts(text);
+	let telaso = search.all[0].url;
+ await reply();
+	let kyuu = await fetchJson (`https://widipe.com/download/ytdl?url=${telaso}`)
+await client.sendMessage(m.chat, {
+  document: {url: kyuu.result.mp3},
+mimetype: "audio/mp3",
+ fileName: `${kyuu.result.title}.mp3`,
+ contextInfo: {
+        externalAdReply: {
+          title: 'Raven-md',
+          body: `${search.all[0].title}`,
+          thumbnailUrl: `${search.all[0].thumbnail}`,
+          sourceUrl: `${telaso}`,
+          mediaType: 2,
+          showAdAttribution: true,
+          renderLargerThumbnail: false
         }
-        try {
-            const {
-                videos
-            } = await yts(text);
-            if (!videos || videos.length <= 0) {
-                reply(`No Matching videos found for : *${args[0]}*!!`)
-                return;
-            }
-            let urlYt = videos[0].url
-            let infoYt = await ytdl.getInfo(urlYt);
-
-
-            const getRandonm = (ext) => {
-                return `${Math.floor(Math.random() * 10000)}${ext}`;
-            };
-            let titleYt = infoYt.videoDetails.title;
-            let randomName = getRandonm(".mp3");
-            const stream = ytdl(urlYt, {
-                    filter: (info) => info.audioBitrate == 160 || info.audioBitrate == 128,
-                })
-                .pipe(fs.createWriteStream(`./${randomName}`));
-            console.log("Audio downloading ->", urlYt);
-            // reply("Downloading.. This may take upto 5 min!");
-            await new Promise((resolve, reject) => {
-                stream.on("error", reject);
-                stream.on("finish", resolve);
-            });
-
-            let stats = fs.statSync(`./${randomName}`);
-            let fileSizeInBytes = stats.size;
-            // Convert the file size to megabytes (optional)
-            let fileSizeInMegabytes = fileSizeInBytes / (1024 * 1024);
-            console.log("Audio downloaded ! \n Size: " + fileSizeInMegabytes);
-
-
-                await client.sendMessage(
-                    m.chat, {
-                        document: fs.readFileSync(`./${randomName}`),
-                        mimetype: "audio/mpeg",
-                        fileName: titleYt + ".mp3",
-                    }, {
-                        quoted: m
-                    }
-                );
-
-            fs.unlinkSync(`./${randomName}`);
-        } catch (e) {
-            m.reply(e.toString())
-        }
-    }
-break;
+      }
+    }, { quoted: m });
+    client.sendMessage(m.chat, { react: { text: '✅', key: m.key }})
+}
+break  
 
 case 'ytsearch':
     case 'yts': {
