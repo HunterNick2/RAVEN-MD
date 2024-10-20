1bc26762d3ca1bf2c6f5b0f72d87021bb5b784db
Nick Hunter <137078621+HunterNick2@users.noreply.github.com>
2024-10-20 08:43:04 +0300

    Update dreaded.js

--- dreaded.js
+++ dreaded.js
@@ -500,6 +500,36 @@ reply('An error occured while communicating with the APIs\n' + e);
 }
 break;
 
+   case "gpt2": {
+const { G4F } = require("g4f"); 
+if (!text) return m.reply("What's your question ?");
+
+try {
+
+
+const GPT = new G4F(); 
+
+const messages = [
+        { role: "system", content: "You're a whatsapp bot called Raven AI that processes users text and accepts commands. You work courtesy of bing from Microsoft."},
+        { role: "user", content: text}
+];
+
+
+GPT.chatCompletion(messages)
+  .then(result => {
+   
+    m.reply(result);
+  });
+
+} catch (e) {
+console.log(e);
+
+ m.reply("Error occured") }
+
+
+
+}
+break;
 case "compile-js":
 if (!text && !m.quoted) throw 'Quote/tag a Js code to compile.';
 
