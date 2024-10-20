4461115ee0bf3ea13dc20ab88f99073a0d3686a3
Nick Hunter <137078621+HunterNick2@users.noreply.github.com>
2024-10-20 16:07:49 +0300

    Update dreaded.js

--- dreaded.js
+++ dreaded.js
@@ -598,7 +598,7 @@ if (isTele) {
 
 let fta2 = await client.downloadAndSaveMediaMessage(q)
 
-    let link = await uploadtoimgur(fta2)
+    let link = await Telegraph(fta2)
 
     const fileSizeMB = (mediaBuffer.length / (1024 * 1024)).toFixed(2)
 
