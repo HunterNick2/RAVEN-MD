0e83f0497d4993fac3cbb34745726fe6934ae6f7
Nick Hunter <137078621+HunterNick2@users.noreply.github.com>
2024-10-20 14:22:47 +0300

    Update dreaded.js

--- dreaded.js
+++ dreaded.js
@@ -573,20 +573,47 @@ const rel = await quote(xf, pushname, pppuser)
             }
    break;
 		      
-            case 'upload': case 'url': {
+            case "upload": case "url": {
+ const fs = require("fs");
+const path = require('path');
+
+const util = require("util");
+
+let q = m.quoted ? m.quoted : m
+let mime = (q.msg || q).mimetype || ''
+
+if (!mime) return m.reply('Quote an image or video')
+
+let mediaBuffer = await q.download()
+
+  if (mediaBuffer.length > 10 * 1024 * 1024) return m.reply('Media is too large.')
+
+
+
+
+let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
+
+
+if (isTele) {
+
+let fta2 = await client.downloadAndSaveMediaMessage(q)
+
+    let link = await uploadtoimgur(fta2)
+
+    const fileSizeMB = (mediaBuffer.length / (1024 * 1024)).toFixed(2)
+
+    m.reply(`Media Link:-\n\n${link}`)
+  } else {
+    m.reply(`Error occured...`)
+  }
+              
+      
+          
                 
-                let fta2 = await client.downloadAndSaveMediaMessage(qmsg)
-                if (/image/.test(mime)) {
-                    let fta = await TelegraPh(fta2)
-                    reply(util.format(fta))
-                } else if (!/image/.test(mime)) {
-                    let fta = await UploadFileUgu(fta2)
-                    reply(util.format(fta))
-                }
-                await fs.unlinkSync(fta2)
+
 
             }
-            break;
+      break;
 case 'attp':
                 if (!q) return reply('I need text;')
               
