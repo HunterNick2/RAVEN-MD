1e006dad1076a0d761d4fbae2f5067634ee84a48
Nick Hunter <137078621+HunterNick2@users.noreply.github.com>
2024-10-21 16:59:27 +0300

    Update dreaded.js

--- dreaded.js
+++ dreaded.js
@@ -1576,66 +1576,50 @@ case 'ytsearch':
     }
     break;
 
-case 'ytmp3':
-case 'yta': {
-      const getRandommmm = (ext) => {
-            return `${Math.floor(Math.random() * 10000)}${ext}`;
-        };
-        if (args.length === 0) {
-            reply(`URL is empty! \nSend ${prefix}ytmp3 url`);
-            return;
-        }
-        try {
-            let urlYt = args[0];
-            if (!urlYt.startsWith("http")) {
-                reply(`Youtube link?`);
-                return;
-            }
-            let infoYt = await ytdl.getInfo(urlYt);
-            //30 MIN
-            if (infoYt.videoDetails.lengthSeconds >= 1800) {
-                reply(`Video too big!`);
-                return;
-            }
-            let titleYt = infoYt.videoDetails.title;
-            let randomName = getRandommmm(".mp3");
-            const stream = ytdl(urlYt, {
-                    filter: (info) => info.audioBitrate == 160 || info.audioBitrate == 128,
-                })
-                .pipe(fs.createWriteStream(`./${randomName}`));
-            console.log("Audio downloading ->", urlYt);
-            // reply("Downloading.. This may take upto 5 min!");
-            await new Promise((resolve, reject) => {
-                stream.on("error", reject);
-                stream.on("finish", resolve);
-            });
-            
-            let stats = fs.statSync(`./${randomName}`);
-            let fileSizeInBytes = stats.size;
-            // Convert the file size to megabytes (optional)
-            let fileSizeInMegabytes = fileSizeInBytes / (1024 * 1024);
-            console.log("Audio downloaded ! Size: " + fileSizeInMegabytes);
-            if (fileSizeInMegabytes <= 40) {
-                //sendFile(from, fs.readFileSync(`./${randomName}`), msg, { audio: true, jpegThumbnail: (await getBuffer(dl.meta.image)).buffer, unlink: true })
-                await client.sendMessage(
-                    from, {
-                        document: fs.readFileSync(`./${randomName}`),
-                        mimetype: "audio/mpeg",
-                        fileName: titleYt + ".mp3",
-                    }, {
-                        quoted: m
-                    }
-                );
-            } else {
-                reply(`File size bigger than 40mb.`);
-            }
-            fs.unlinkSync(`./${randomName}`);
-        } catch (e) {
-            reply(e.toString())
-        }
-    }
+case "ytmp3": {
+if (!text) return m.reply("Provide a valid YouTube link!")
 
-break  
+	let urls = text.match(/(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch\?v=|v\/|embed\/|shorts\/|playlist\?list=)?)([a-zA-Z0-9_-]{11})/gi);
+	if (!urls) return m.reply('Is this a YouTube link ?');
+	let urlIndex = parseInt(text) - 1;
+	if (urlIndex < 0 || urlIndex >= urls.length)
+		return m.reply('Invalid URL index');
+	await downloadMp3(urls);
+
+
+
+
+
+async function downloadMp3 (link) {
+try {
+
+let data = await fetchJson (`https://widipe.com/download/ytdl?url=${link}`)
+
+await client.sendMessage(m.chat, {
+ audio: {url: data.result.mp3},
+mimetype: "audio/mp3",
+ fileName: `${data.result.title}.mp3`
+ 
+    }, { quoted: m });
+
+await client.sendMessage(m.chat, {
+ document: {url: data.result.mp3},
+mimetype: "audio/mp3",
+ fileName: `${data.result.title}.mp3`
+ 
+    }, { quoted: m });
+  } catch (error) {
+    console.error('Error fetching the song:', error);
+    await m.reply(`Error.`)
+  }
+}
+
+
+
+
+}
+break;
+  
 case 'ytmp4':
 case 'ytvideo':
 case 'ytv':
